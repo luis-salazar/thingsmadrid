b3e948c8885e03d3136d85d3a5449d51969e164b
luis-salazar <luissalazarjurado@gmail.com>
2014-06-16 08:27:27 +0200

    html delete trush

--- wp-content/themes/twentythirteen/functions.php
+++ wp-content/themes/twentythirteen/functions.php
@@ -562,3 +562,10 @@ function google_analytics_tracking(){
 <?php } ?>
 <?php
 add_action('wp_footer','google_analytics_tracking');
+
+// optimizacion wordpress - delete html trush
+remove_action( 'wp_head', 'wp_generator' ) ; 
+remove_action( 'wp_head', 'wlwmanifest_link' ) ; 
+remove_action( 'wp_head', 'rsd_link' ) ;
+remove_action( 'wp_head', 'feed_links', 2 ); 
+remove_action( 'wp_head', 'feed_links_extra', 3 );
