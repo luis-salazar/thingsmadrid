1f6d10ff782ed4c2292128cebb0bd076a31279c6
luis-salazar <luissalazarjurado@gmail.com>
2014-06-23 18:33:04 +0200

    changes header + social icons

--- wp-content/themes/twentythirteen/header.php
+++ wp-content/themes/twentythirteen/header.php
@@ -15,11 +15,20 @@
 <body <?php body_class(); ?>>
 	<div id="page" class="hfeed site">
 		<header id="masthead" class="site-header" role="banner">
-			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			</a>
-
+			<div id="slogan_wrapper">
+				<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+					<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+				</a>
+			</div>
+			<div id="social-icons">
+				<ul>
+					<li class="pinterest"><a title="Look at pictures from Madrid" target="_blank" href="http://www.pinterest.com/thingsmadrid/"><i class="genericon-pinterest"></i></a></li>
+					<li class="twitter"><a title="Follow @thingsmadrid on Twitter" target="_blank" href="https://twitter.com/ThingsMadrid"><i class="genericon-twitter"></i></a></li>
+					<li class="facebook"><a title="Share on Facebook" target"_blank" href="https://www.facebook.com/thingsmadrid"><i class="genericon-facebook"></i></a></li>
+					<li class="google"><a title="Share on Google Plus" target="_blank" href="https://plus.google.com/107682883598168469224/posts"><i class="genericon-googleplus"></i></a></li>
+				</ul>
+			</div>
 			<div id="navbar" class="navbar">
 				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
 					<h3 class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></h3>
