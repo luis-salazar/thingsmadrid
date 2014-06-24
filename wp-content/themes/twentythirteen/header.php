c97b86db8b6bc1a1a5b21c8a89648059a13238e7
luis-salazar <luissalazarjurado@gmail.com>
2014-06-24 08:45:34 +0200

    pinterest domain verification

--- wp-content/themes/twentythirteen/header.php
+++ wp-content/themes/twentythirteen/header.php
@@ -6,6 +6,7 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<meta name="p:domain_verify" content="1ce3304debd37e2c175a42d8da359b03"/>
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 <![endif]-->
